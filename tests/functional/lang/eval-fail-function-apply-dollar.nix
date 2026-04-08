d34d3c18c62f417734564447271964b09ab4e485
Max <max@privatevoid.net>
2026-04-08 23:56:43 +0200

    remove apply-dollar tests

--- tests/functional/lang/eval-fail-function-apply-dollar.nix
+++
@@ -1,6 +0,0 @@
-let
-  a = 5;
-  b = 1;
-
-  add = a: b: a + b;
-in add a $ a $ b
--- tests/functional/lang/eval-okay-function-apply-dollar.nix
+++
@@ -1,9 +0,0 @@
-let
-  a = 5;
-  b = 1;
-  c = 2;
-  concat = a: b: "${a}${b}";
-  nested.attr.ab = 10;
-
-  add = a: b: a + b;
-in add nested.attr.${concat "a" "b"} $ add b $ add a c
